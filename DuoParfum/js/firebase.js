95127cf67aa6b88aa9fd2f560212f5421d45cf40
Guilherme Serraglio <128415162+GuilhermeSerraglio@users.noreply.github.com>
2025-07-21 20:35:14 -0400

    Update firebase.js

--- DuoParfum/js/firebase.js
+++ DuoParfum/js/firebase.js
@@ -1,28 +1,20 @@
-// Importações necessárias do Firebase modular
+// js/firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Configurações do seu projeto Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyC9x1yRCeTIlx9vJyZQxa6_4tK6Oc1eNXg",
-  authDomain: "duoparfum-a0108.firebaseapp.com",
-  projectId: "duoparfum-a0108",
-  storageBucket: "duoparfum-a0108.firebasestorage.app",
-  messagingSenderId: "747127124075",
-  appId: "1:747127124075:web:443a01002c8698e057412e",
-  measurementId: "G-QKV7PG5DRD"
+  apiKey: "AIzaSyBt4KCTdAzJK2j6C_sH2eUllD3LOFiD6lI",
+  authDomain: "duoparfum.firebaseapp.com",
+  projectId: "duoparfum",
+  storageBucket: "duoparfum.appspot.com",
+  messagingSenderId: "859055985734",
+  appId: "1:859055985734:web:b2601e4ee0c84cf6dd2e34"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-
-// Firestore
 const db = getFirestore(app);
-
-// Auth
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-// Exporta para outros arquivos JS
 export { db, auth, provider };
